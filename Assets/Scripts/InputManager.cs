c5c53f1bab52a78b2b5f5ce3d5a47ac4655f1612
SakRey63 <152211412+SakRey63@users.noreply.github.com>
2025-01-18 13:25:32 +0300

    The finished version 1.0

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,31 @@
+using System;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] private Camera _camera;
+    [SerializeField] private SceneManager _sceneManager;
     
+    private Ray _ray;
+
+    public event Action OnHit;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _ray = _camera.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit hit;
+            
+            Physics.Raycast(_ray, out hit, Mathf.Infinity);
+    
+            if (hit.collider.GetComponent<CubeController>())
+            {
+                OnHit?.Invoke();
+                
+                hit.collider.GetComponent<CubeController>().DestroyCube(_sceneManager.Dell);
+            }
+        }
+    }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,24 +3,99 @@ using Random = UnityEngine.Random;
 
 public class SceneManager : MonoBehaviour
 {
-    [SerializeField] private Transform[] _points;
+    [SerializeField] private Transform _point;
     [SerializeField] private GameObject _cube;
     [SerializeField] private int _minLimit;
     [SerializeField] private int _maxLimit;
+    [SerializeField] private int _maxChanceSeparation = 101;
     [SerializeField] private int _chanceSeparation = 100;
+    [SerializeField] private int _minChanceSeparation = 0;
+    [SerializeField] private CubeController _controller;
+    [SerializeField] private InputManager _inputManager;
+    
+    private float _scaleCube = 3f;
+    private int _indexCube = 0;
+    private bool _onDel;
 
+    public bool Dell => _onDel;
+    
+    private void OnEnable()
+    {
+        _inputManager.OnHit += Separation;
+        
+    }
+
+    private void OnDisable()
+    {
+        _inputManager.OnHit -= Separation;
+    }
+    
     private void Start()
     {
         SpawnCubes();
     }
 
+    private void ReduceSizeCube()
+    {
+        float bisection = 2f;
+
+        _scaleCube = _scaleCube / bisection;
+    }
+
+    private void Separation()
+    {
+        int numberChance = RandomNumber(_minChanceSeparation, _maxChanceSeparation);
+        
+        if (numberChance <= _chanceSeparation)
+        {
+            _onDel = true;
+            
+            SpawnCubes();
+            
+            ReducingChanceDivision();
+        }
+        else
+        {
+            _onDel = false;
+        }
+    }
+    
+    private void ReducingChanceDivision()
+    {
+        int bisection = 2;
+        
+        _chanceSeparation = _chanceSeparation / bisection;
+    }
+    
     private void SpawnCubes()
     {
-        int random = Random.Range(_minLimit, _maxLimit);
+        _indexCube++;
         
-        for (int i = 0; i < random; i++)
+        int cubeCount = RandomNumber(_minLimit, _maxLimit);
+        
+        for (int i = 0; i < cubeCount; i++)
         {
-            Instantiate(_cube, _points[Random.Range(0, _points.Length-1)]);
+            Instantiate(MakeChangesCube(_cube), _point);
+
+            _point.parent = null;
         }
+        
+        ReduceSizeCube();
+    }
+
+    private GameObject MakeChangesCube(GameObject cube)
+    {
+        _controller = cube.GetComponent<CubeController>();
+            
+        _controller.AssigningParameters(_indexCube, _scaleCube);
+
+        return cube;
+    }
+
+    private int  RandomNumber(int minNumber, int maxNumber)
+    {
+        int random = Random.Range(minNumber, maxNumber);
+
+        return random;
     }
 }
